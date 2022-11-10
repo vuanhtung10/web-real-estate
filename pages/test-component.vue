927a098de389f2f5bc8ab30d637df0158b11e51a
Canhta9 <cuonganh301095@gmail.com>
2022-11-10 10:23:00 +0700

    CSS input

--- pages/test-component.vue
+++ pages/test-component.vue
@@ -1,7 +1,74 @@
 <template>
   <div>
     <div>test</div>
-    <input-with-validation v-model="email" label="email" />
+    <input-with-validation v-model="email" placeholder="Enter your Email">
+      <button class="ml-4">
+        <svg
+          class="h-6 w-6 ml-4 my-auto text-blueGray-300"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+          ></path>
+        </svg>
+      </button>
+    </input-with-validation>
+
+    <input-with-validation
+      v-model="password"
+      :type="showPassword ? `text` : `password`"
+      placeholder="Enter your password"
+    >
+      <button
+        v-if="!showPassword"
+        class="ml-4"
+        @click="showPassword = !showPassword"
+      >
+        <svg
+          class="h-6 w-6 my-auto text-blueGray-300"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          ></path>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+          ></path>
+        </svg>
+      </button>
+      <button v-else class="ml-4" @click="showPassword = !showPassword">
+        <svg
+          class="h-6 w-6 ml-4 my-auto text-blueGray-300"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+          ></path>
+        </svg>
+      </button>
+    </input-with-validation>
+
     <div>
       <pagination
         :total-item="totalItem"
@@ -9,8 +76,6 @@
         @handleSizeChange="handleSizeChange"
       ></pagination>
     </div>
-    <div></div>
-    <input-with-validation v-model="email" label="email" />
     <switch-with-validation v-model="status" />
     <checkbox-with-validation v-model="value" :value-options="listOptions" />
     <!-- <filter-check-box label="Size" class="w-[290px]" /> -->
@@ -42,6 +107,7 @@ export default {
         { text: 'Four', value: 4 },
         { text: 'Five', value: 5 },
       ],
+      showPassword: false,
     }
   },
   watch: {
